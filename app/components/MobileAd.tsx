41b9dc4294a84328d4db17c83d49625904cce46a
Mike Stum <mike@realchoicerealestate.com>
2026-05-17 14:57:20 -0400

    moving mobile ads

--- app/components/MobileAd.tsx
+++ app/components/MobileAd.tsx
@@ -10,10 +10,10 @@ export default function MobileAd({
   pageType?: "memorial" | "create" | "home" | "search" | "personal";
 }) {
   return (
-    <div className="lg:hidden">
-      <div className="[&>div]:!flex [&>div]:!w-full [&>div]:!items-stretch [&>div>div]:!static [&>div>div]:!w-full"
-        <SideAd memorialZip={memorialZip} pageType={pageType} />
-      </div>
+  <div className="lg:hidden">
+    <div className="[&>div]:!flex [&>div]:!w-full [&>div]:!items-stretch [&>div>div]:!static [&>div>div]:!w-full">
+      <SideAd memorialZip={memorialZip} pageType={pageType} />
     </div>
-  );
+  </div>
+);
 }
