64ab3ca7762d9b3e19513943a7e7c42046f6f493
Mike Stum <mike@realchoicerealestate.com>
2026-06-14 15:06:29 -0400

    Temp price change

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -50,7 +50,7 @@ if (plan === "extra_videos") {
 }
 
 if (checkoutType !== "upgrade") {
-  if (plan === "basic") safeUnitAmount = 9900;
+  if (plan === "basic") safeUnitAmount = 100;
   if (plan === "plus") safeUnitAmount = 12495;
   if (plan === "premium") safeUnitAmount = 100;
 }
